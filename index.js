d941ff3313432f50fa4bd2a44b6914bba7c7196d
piyush1997jagtap <49496878+piyush1997jagtap@users.noreply.github.com>
2019-04-10 12:46:11 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,67 +1,22 @@
-// DOM elements
-const guideList = document.querySelector('.guides');
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
-const accountDetails = document.querySelector('.account-details');
-const adminItems = document.querySelectorAll('.admin');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-const setupUI = (user) => {
-  if (user) {
-    if (user.admin) {
-      adminItems.forEach(item => item.style.display = 'block');
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check request is made by an admin
+  if ( context.auth.token.admin !== true ) {
+    return { error: 'Only admins can add other admins' }
+  }
+  // get user and add admin custom claim
+  return admin.auth().getUserByEmail(data.email).then(user => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    })
+  }).then(() => {
+    return {
+      message: `Success! ${data.email} has been made an admin.`
     }
-    // account info
-    db.collection('users').doc(user.uid).get().then(doc => {
-      const html = `
-        <div>Logged in as ${user.email}</div>
-        <div>${doc.data().bio}</div>
-        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
-      `;
-      accountDetails.innerHTML = html;
-    });
-    // toggle user UI elements
-    loggedInLinks.forEach(item => item.style.display = 'block');
-    loggedOutLinks.forEach(item => item.style.display = 'none');
-  } else {
-    // clear account info
-    accountDetails.innerHTML = '';
-    // toggle user elements
-    adminItems.forEach(item => item.style.display = 'none');
-    loggedInLinks.forEach(item => item.style.display = 'none');
-    loggedOutLinks.forEach(item => item.style.display = 'block');
-  }
-};
-
-// setup guides
-const setupGuides = (data) => {
-
-  if (data.length) {
-    let html = '';
-    data.forEach(doc => {
-      const guide = doc.data();
-      const li = `
-        <li>
-          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
-          <div class="collapsible-body white"> ${guide.content} </div>
-        </li>
-      `;
-      html += li;
-    });
-    guideList.innerHTML = html
-  } else {
-    guideList.innerHTML = '<h5 class="center-align">Login to view domains</h5>';
-  }
-  
-
-};
-
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
-
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
-
+  }).catch(err => {
+    return err;
+  });
 });
